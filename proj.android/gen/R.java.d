9ca855c69d9116c2bf55a5985fca47e431daab88
AIRIA <chaibingcheng0305@gmail.com>
2015-03-05 15:58:14 +0800

    change ingore file

---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+/Users/AIRIA/dev/projects/Zodiac/proj.android/gen/com/youzhong/zodiac/R.java \
+ : /Users/AIRIA/dev/projects/Zodiac/proj.android/res/drawable-hdpi/icon.png \
+/Users/AIRIA/dev/projects/Zodiac/proj.android/res/drawable-ldpi/icon.png \
+/Users/AIRIA/dev/projects/Zodiac/proj.android/res/drawable-mdpi/icon.png \
+/Users/AIRIA/dev/projects/Zodiac/proj.android/res/values/strings.xml \
+/Users/AIRIA/dev/projects/Zodiac/proj.android/bin/AndroidManifest.xml \
